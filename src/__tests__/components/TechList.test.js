04626cc5669de4c9e1f86187e09b7e4acc4b10e0
Gabriel Lima <gabriellfsouza@gmail.com>
2019-09-10 04:36:11 -0300

    chore: first tdd test -> phase 2, green

--- src/__tests__/components/TechList.test.js
+++ src/__tests__/components/TechList.test.js
@@ -5,9 +5,13 @@ import TechList from "~/components/TechList";
 
 describe("TechList component", () => {
   it("should be able to add new tech", () => {
-    const { getByText, getByTestId } = render(<TechList />);
+    const { getByText, getByTestId, debug } = render(<TechList />);
+
+    debug();
 
     fireEvent.click(getByText("Adicionar"));
+
+    debug();
 
     expect(getByTestId("tech-list")).toContainElement(getByText("Node.js"));
   });
